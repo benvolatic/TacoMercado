d560963160cbf7cd9ad4aaa6158a54f3199198dc
benvolatic <ben.volatic@gmail.com>
2024-07-30 14:27:21 -0700

    Type Safe Components

---
+++ TacoMercado/src/components/ProductListItem.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "../constants/Colors";
+import { Product } from "../types";
+import products from "../../assets/data/products";
+
+export const defaultTacoImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: product.image || defaultTacoImage }}
+        style={styles.image}
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    color: Colors.dark.text,
+    fontSize: 20,
+    fontWeight: 600,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
